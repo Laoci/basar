3732f249c9f2619a4a29c30670561c60f6c507e7
ray lim <ziyang.lim89@gmail.com>
2021-09-14 17:47:46 +0800

    created order table with user reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_09_14_090309) do
+ActiveRecord::Schema.define(version: 2021_09_14_094506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "item", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "quantity"
+    t.string "photo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.boolean "delivered"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.integer "postal_code"
+    t.string "address"
+    t.integer "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,23 +55,6 @@ ActiveRecord::Schema.define(version: 2021_09_14_090309) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.integer "quantity"
-    t.string "photo"
-  end
-  
-  create_table "shops", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.integer "postal_code"
-    t.string "address"
-    t.integer "phone_number"
 
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+  add_foreign_key "orders", "users"
 end
