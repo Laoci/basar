abb0b55dc8f971d2a160fc50f03a3df92d490f55
Ran Ci <ciran1990@protonmail.com>
2021-09-16 16:31:57 +0800

    heroku for Thu 04

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -14,7 +14,7 @@ class ShopsController < ApplicationController
       return nil
     end
 
-    if @shops.empty? || query_value.empty?
+    if @shops.empty? || query_value.to_s.empty?
       redirect_to root_path, alert: alert
       return nil
     end
