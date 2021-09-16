9095a2786ca447d9e0d7bd9020cd9f9ab0f23b2d
Audrey Kon <audz@kon.sg>
2021-09-16 18:19:45 +0800

    added carousel first but gonna make it work later

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,20 @@ shop2 = Shop.create!(name: "shop 2", photo: "insert url", postal_code: 100002, a
 shop3 = Shop.create!(name: "shop 3", photo: "insert url", postal_code: 100003, address: "insert address", phone_number: "12345678" )
 
 puts "Creating items..."
-banana = Item.create!(name: "Banana", price: 1.25, quantity: 3, photo: "insert url", shop: shop1)
-apple = Item.create!(name: "Apple", price: 0.90, quantity: 3, photo: "insert url", shop: shop1)
-carrot = Item.create!(name: "Carrot", price: 1.05, quantity: 3, photo: "insert url", shop: shop2)
-durian = Item.create!(name: "Durian", price: 5.50, quantity: 3, photo: "insert url", shop: shop2)
-egg = Item.create!(name: "Egg", price: 3.25, quantity: 3, photo: "insert url", shop: shop3)
-pear = Item.create!(name: "Pear", price: 1.5, quantity: 3, photo: "insert url", shop: shop3)
+banana = Item.create!(name: "Banana", price: 1.25, quantity: 20, photo: "insert url", shop: shop1)
+apple = Item.create!(name: "Apple", price: 0.90, quantity: 20, photo: "insert url", shop: shop1)
+carrot = Item.create!(name: "Carrot", price: 1.05, quantity: 11, photo: "insert url", shop: shop1)
+durian = Item.create!(name: "Durian", price: 5.50, quantity: 21, photo: "insert url", shop: shop1)
+egg = Item.create!(name: "Egg", price: 3.10, quantity: 3, photo: "insert url", shop: shop1)
+banana = Item.create!(name: "Banana", price: 1.20, quantity: 33, photo: "insert url", shop: shop2)
+apple = Item.create!(name: "Apple", price: 0.90, quantity: 2, photo: "insert url", shop: shop2)
+carrot = Item.create!(name: "Carrot", price: 1.10, quantity: 2, photo: "insert url", shop: shop2)
+durian = Item.create!(name: "Durian", price: 5.10, quantity: 23, photo: "insert url", shop: shop2)
+egg = Item.create!(name: "Egg", price: 3.25, quantity: 30, photo: "insert url", shop: shop2)
+banana = Item.create!(name: "Banana", price: 1.20, quantity: 22, photo: "insert url", shop: shop3)
+apple = Item.create!(name: "Apple", price: 0.95, quantity: 11, photo: "insert url", shop: shop3)
+carrot = Item.create!(name: "Carrot", price: 1.00, quantity: 22, photo: "insert url", shop: shop3)
+durian = Item.create!(name: "Durian", price: 5.00, quantity: 44, photo: "insert url", shop: shop3)
+egg = Item.create!(name: "Egg", price: 3.25, quantity: 22, photo: "insert url", shop: shop3)
 
 puts "Done seeding!"
