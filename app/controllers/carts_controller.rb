3499c42cca0cfb4e18de5952335e6ec2493f20dc
Ran Ci <ciran1990@protonmail.com>
2021-09-20 13:06:36 +0800

    after successful payment return to order summary pate

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,19 +2,18 @@ class CartsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
-
-     data = JSON.parse(request.body.read)
-     cart_object = JSON.parse(data)
+    data = JSON.parse(request.body.read)
+    cart_object = JSON.parse(data)
     # [{"itemId"=>"5", "itemCount"=>"5"}]
-     @cart_objects = cart_object.map do |cart_item|
-        item = Item.find(cart_item['itemId'])
-        {
-          item_id: item.id,
-          item_name: item.name,
-          item_count: cart_item['itemCount'],
-          item_price: item.price * cart_item['itemCount'].to_i
+    @cart_objects = cart_object.map do |cart_item|
+      item = Item.find(cart_item['itemId'])
+      {
+        item_id: item.id,
+        item_name: item.name,
+        item_count: cart_item['itemCount'],
+        item_price: item.price * cart_item['itemCount'].to_i
       }
-     end
+    end
     respond_to do |format|
       format.html
     end
